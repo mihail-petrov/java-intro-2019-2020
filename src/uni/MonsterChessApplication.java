051a728f4fad4d71a9d9110012a4e28629ea1d38
Emil Doychev <e.doychev@uni-plovdiv.net>
2020-05-06 09:11:01 +0300

    Print board labels

--- src/uni/MonsterChessApplication.java
+++ src/uni/MonsterChessApplication.java
@@ -2,15 +2,19 @@ package uni;
 
 public class MonsterChessApplication {
 	public static final String FIG_DWARF       = "Dw";
-	public static final String FIG_DONKEY      = "D";
-	public static final String FIG_QUEEN       = "Q";
-	public static final String FIG_KING        = "K";
-	public static final String FIG_GUN         = "M";
-	public static final String EMPTY_POSITION  = "X";
+	public static final String FIG_DONKEY      = "D ";
+	public static final String FIG_QUEEN       = "Q ";
+	public static final String FIG_KING        = "K ";
+	public static final String FIG_GUN         = "M ";
+	public static final String EMPTY_POSITION  = " X ";
 	public static final String WHITE           = "w";
 	public static final String BLACK           = "b";
-	public static final String COL_DIVIDER     = "+";
-	public static final String ROW_DIVIDER     = "===============================";
+	public static final String COL_DIVIDER     = " + ";
+	public static final String ROW_DIVIDER     = "=====================================";
+	public static final String V_SIGN_DIVIDER  = " | ";
+	public static final char   START_LABEL     = 'A';
+
+
 
 	public static void main(String[] args) {
 
@@ -36,13 +40,36 @@ public class MonsterChessApplication {
 		println();
 		println(ROW_DIVIDER);
 		for (int i = 0; i < board.length; i++) {
+
+			print(START_LABEL + i);
+			print(V_SIGN_DIVIDER);
+
 			for (int j = 0; j < board[i].length; j++) {
 				print(board[i][j]);
-				print(COL_DIVIDER);
+				if (j < board[i].length - 1) {
+					print(COL_DIVIDER);
+				}
 			}
 			println();
 			println(ROW_DIVIDER);
 		}
+
+		printHorizontalLabels(board[0].length);
+	}
+
+	/**
+	 * Извеждане на хоризонталните етикети на дъската
+	 *
+	 * @param boardSize хоризонталния размер на дъската
+	 */
+	public static void printHorizontalLabels(int boardSize) {
+		print("    ");
+		for (int i = 0; i < boardSize; i++) {
+			print(" ");
+			print(START_LABEL + i);
+			print("    ");
+		}
+		println();
 	}
 
 	/**
@@ -91,6 +118,15 @@ public class MonsterChessApplication {
 	 */
 	public static void println(String message) {
 		System.out.println(message);
+	}
+
+	/**
+	 * Извежда в конзолата единичен символ
+	 *
+	 * @param sign единичният символ
+	 */
+	public static void print(int sign) {
+		System.out.print((char)sign);
 	}
 
 	/**
